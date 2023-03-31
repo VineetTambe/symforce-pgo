83cb00da8145f0d6b029d970aeade3615441cf44
VineetTambe <tambevineet@gmail.com>
2023-03-30 22:20:19 -0400

    added some stuff

---
+++ pose_graph_opt/src/main.py
@@ -0,0 +1,9 @@
+import symforce
+symforce.set_epsilon_to_symbol()
+
+import numpy as np
+from symforce.values import Values
+import symforce.symbolic as sf
+
+# for optimisation
+from symforce.opt.factor import Factor
